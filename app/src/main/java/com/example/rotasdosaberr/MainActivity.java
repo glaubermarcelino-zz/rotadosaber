8731807a203708c08c234158707c8592ae86cf38
GTS Soluções Tecnológicas <gmsoft.informatica@gmail.com>
2021-01-17 16:54:39 -0300

    feat: Adicionado layout para o bottom-bar-navigation

--- app/src/main/java/com/example/rotasdosaberr/MainActivity.java
+++ app/src/main/java/com/example/rotasdosaberr/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.rotasdosaberr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        bottomNavigationView = findViewById(R.id.bottomnav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -22,5 +30,16 @@ public class MainActivity extends AppCompatActivity {
         }, 5000);
     }
 
-
+private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment =null;
+        /*switch(item.getItemId()){
+            case R.id.cursos:
+                fragment = new AndroidFragment();
+        }
+        */
+        return false;
+    }
+};
 }
